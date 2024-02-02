13d91281c656f5a02e8d8211fa967c0d3920f6fc
Veselov Nikolay <veselovnd@gmail.com>
2024-02-02 20:19:12 +0300

    HW01: private constructor moved up

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -5,14 +5,14 @@ import java.time.format.DateTimeFormatter;
 
 public class TimeUtil {
 
+    private TimeUtil() {
+        throw new AssertionError("No instance for util class");
+    }
+
     public static final DateTimeFormatter DATETIME_FMT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
-    }
-
-    private TimeUtil() {
-        throw new AssertionError("No instance for util class");
     }
 
 }
