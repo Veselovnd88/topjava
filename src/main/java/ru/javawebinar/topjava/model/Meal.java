674b27be3e434e2267c83d869ad165071d261a64
Veselov Nikolay <veselovnd@gmail.com>
2024-02-24 14:54:04 +0300

    HW04: preparations

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,39 @@
 package ru.javawebinar.topjava.model;
 
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Objects;
 
+@Entity
+@Table(name = "meal")
 public class Meal extends AbstractBaseEntity {
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(min = 4, max = 128)
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @Range(min = 10, max = 10000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
     private User user;
 
     public Meal() {
@@ -78,5 +98,20 @@ public class Meal extends AbstractBaseEntity {
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Meal)) return false;
+        if (!super.equals(object)) return false;
+        Meal meal = (Meal) object;
+        return getCalories() == meal.getCalories() && Objects.equals(getDateTime(), meal.getDateTime())
+                && Objects.equals(getDescription(), meal.getDescription()) && Objects.equals(getUser(), meal.getUser());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getDateTime(), getDescription(), getCalories(), getUser());
     }
 }
