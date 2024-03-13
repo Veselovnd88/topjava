17574797eaf25ff9e4817948c7df7f0ab8361767
Veselov Nikolay <veselovnd@gmail.com>
2024-03-13 17:04:15 +0300

    HW06: added roles, save/update/delete user with roles

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -5,14 +5,18 @@ import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Repository
 @Transactional(readOnly = true)
@@ -26,6 +30,8 @@ public class JdbcUserRepository implements UserRepository {
 
     private final SimpleJdbcInsert insertUser;
 
+    private final SimpleJdbcInsert insertRole;
+
     @Autowired
     public JdbcUserRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.insertUser = new SimpleJdbcInsert(jdbcTemplate)
@@ -34,6 +40,9 @@ public class JdbcUserRepository implements UserRepository {
 
         this.jdbcTemplate = jdbcTemplate;
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+        this.insertRole = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("user_role")
+                .usingColumns("user_id", "role");
     }
 
     @Override
@@ -43,25 +52,43 @@ public class JdbcUserRepository implements UserRepository {
 
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
+            saveRoles(user.getRoles(), newKey.intValue());
             user.setId(newKey.intValue());
         } else if (namedParameterJdbcTemplate.update("""
                    UPDATE users SET name=:name, email=:email, password=:password, 
                    registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id
-                """, parameterSource) == 0) {
-            return null;
+                """, parameterSource) != 0) {
+            deleteRoles(user.id());
+            saveRoles(user.getRoles(), user.id());
+            return user;
         }
-        return user;
+        return null;
+    }
+
+    private void saveRoles(Set<Role> roles, int userId) {
+        insertRole.executeBatch(new MapSqlParameterSource(
+                Map.of("user_id", userId,
+                        "role", roles.stream().map(Enum::toString).toList()))
+        );
     }
 
     @Override
     @Transactional
     public boolean delete(int id) {
-        return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
+        boolean deleted = jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
+        if (deleted) {
+            deleteRoles(id);
+        }
+        return deleted;
+    }
+
+    private void deleteRoles(int userId) {
+        jdbcTemplate.update("DELETE FROM user_role WHERE user_id=?", userId);
     }
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
+        List<User> users = jdbcTemplate.query("SELECT * FROM users LEFT JOIN user_role ON id=? WHERE id=?", ROW_MAPPER, id);
         return DataAccessUtils.singleResult(users);
     }
 
